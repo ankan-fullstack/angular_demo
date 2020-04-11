26906c67ab68a1bb70c710f59d84be89285417ae
Ankan <ankanbiswas0@gmail.com>
2020-04-11 14:47:15 +0530

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+
+const routes: Routes = [
+  {
+    path:'',component:FirstComponent
+  },
+  {
+    path:'first',component:FirstComponent
+  },
+  {
+    path:'second',component:SecondComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
